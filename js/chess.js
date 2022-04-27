c33f8f2d71fe05f66fbcef3a1a010624be26a1d4
PebPeb <bmkeen@liberty.edu>
2022-04-26 23:40:33 -0500

    Small Updates

--- js/chess.js
+++ js/chess.js
@@ -2,14 +2,36 @@
 class gameBoard
 {
     constructor() {
-        this.board = new Array(8).fill(new Array(8).fill(null));
-        console.log(this.board);
+        self.boardSize = 8;
+        self.boardScale = 80;
+        self.board = new Array(self.boardSize).fill(new Array(self.boardSize).fill(null));
+
+        this.addEvents();
+        console.log(self.board);
     }
 
     draw() {
-        this.canvas = document.getElementById('gameCanvas');
-        this.ctx = this.canvas.getContext('2d');
+        self.canvas = document.getElementById('gameCanvas');
+        self.ctx = self.canvas.getContext('2d');
+
+        for(let i = 0; i < self.boardSize; i++)
+            for(let j = 0; j < self.boardSize; j++) {
+                if ((i+j)%2)
+                    self.ctx.fillStyle = "red";
+                else
+                    self.ctx.fillStyle = "black";
+                self.ctx.fillRect(i*self.boardScale, j*self.boardScale, self.boardScale, self.boardScale);
+            }
         console.log("Test");
+    }
+
+    printMousePos(event){
+        console.log(event.clientX);
+        console.log(event.clientY);
+    }
+
+    addEvents() {
+        document.addEventListener("click", this.printMousePos);
     }
 
 }
